5e94944cd44cff327b038c3eee2b0189fcb1a6da
oshawgan <othman.shawgan@philips.com>
2022-04-13 22:49:24 +0300

    Firebase setup

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -1,8 +1,32 @@
 import 'package:english_words/english_words.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
+}
+
+class App extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Scaffold(
+                body: Center(
+                    child: Text(snapshot.error.toString(),
+                        textDirection: TextDirection.ltr)));
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MyApp();
+          }
+          return Center(child: CircularProgressIndicator());
+        });
+  }
 }
 
 class MyApp extends StatelessWidget {
